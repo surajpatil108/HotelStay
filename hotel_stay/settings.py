de9c19639703ac9e14442214104d0abc21e5f40f
Suraj Rajput <surajpatil108>
2026-06-30 20:33:09 +0530

    added static

--- hotel_stay/settings.py
+++ hotel_stay/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Where files go on Render
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
